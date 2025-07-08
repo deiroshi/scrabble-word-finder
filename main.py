66dc9d029d93c5d5bf0c7d1703bfdb7a1e47aa12
deiroshi <dasha.seliukova@gmail.com>
2025-07-08 09:46:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,23 +3,23 @@ letters = input("Enter letters: ").upper()
 
 # Load Scrabble dictionary 
 def scrabble_dictionary():
-    with open("scrabble_dictionary.txt", "r") as file:     # Reads the file     # "r" - read mode     # with - automatically closes the file when done
-        words = set()                                      # Create Python set  
-        for line in file:                                  # Goes through every line in the file
-            word = line.strip().upper()                    # Remove \n and convert to uppercase
-            words.add(word)                                # Add the word to the set
+    with open("scrabble_dictionary.txt", "r") as file: # Reads the file     # "r" - read mode     # with - automatically closes the file when done
+        words = set() # Create Python set  
+        for line in file: # Goes through every line in the file
+            word = line.strip().upper() # Remove \n and convert to uppercase
+            words.add(word) # Add the word to the set
         return words                                       
 
 # Find/sort all possible words
 from collections import Counter
 def find_words(letters, scrabble_dictionary):
-    letters_count = Counter(letters)                       # Count letters in the input
-    valid_words = []                                       # Create a list to store valid words
-    for word in scrabble_dictionary:                       # Go through each word in the dictionary
-        word_count = Counter(word)                         # Count letters in the word
+    letters_count = Counter(letters) # Count letters in the input
+    valid_words = [] # Create a list to store valid words
+    for word in scrabble_dictionary: # Go through each word in the dictionary
+        word_count = Counter(word) # Count letters in the word
         if all(word_count[char] <= letters_count[char] for char in word_count):  # Check if each letter in the word can be formed with the input letters
-            valid_words.append(word)                       # If valid, add to the list
-    return sorted (valid_words)                            # Return the list sorted alphabetically
+            valid_words.append(word) # If valid, add to the list
+    return sorted (valid_words) # Return the list sorted alphabetically
 
 results = find_words(letters, scrabble_dictionary())      
 
@@ -27,4 +27,3 @@ results = find_words(letters, scrabble_dictionary())
 print("You can make",len(results),"words from",letters)
 for word in results:
     print("-", word)
-
